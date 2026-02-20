1f44de6174b115e35c5c1c059883e86033b8ec7e
Hacker-Anirudh <151405192+Hacker-Anirudh@users.noreply.github.com>
2026-02-20 00:01:05 +0000

    Enable BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,5 +173,7 @@ WIFI_DRIVER_FW_PATH_STA     := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # inherit from the proprietary version
 include vendor/samsung/msm8974-common/BoardConfigVendor.mk
